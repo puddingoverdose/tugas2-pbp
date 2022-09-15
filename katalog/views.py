f8c6d62c90f7ba9e1a6e0e5dfa63d0b20d42b65c
Ridho Mulia <ridho050302@gmail.com>
2022-09-15 10:57:28 +0700

    update

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # TODO: Create your views here.
+def show_catalog(request):
+    data_catalogue = CatalogItem().objects.all()
+    context = {
+        'list_katalog' : data_catalogue
+    }
+    return render(request, 'katalog.html', context)
